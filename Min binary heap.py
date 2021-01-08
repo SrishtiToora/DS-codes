6241b5053dd95ae6ac9eedbdc66128bfcd9377de
SrishtiToora <77147148+SrishtiToora@users.noreply.github.com>
2021-01-08 18:53:38 +0530

    Add files via upload

---
+++ Min binary heap.py
@@ -0,0 +1,68 @@
+class binaryheap:
+    def __init__(self):
+        self.heapList=[0]
+        self.currentSize=0
+
+    def percUp(self,i):
+        while i//2>0:
+            if self.heapList[i]<self.heapList[i//2]:
+                tmp=self.heapList[i//2]
+                self.heapList[i//2]=self.heapList[i]
+                self.heapList[i]=tmp
+            i=i//2
+
+    def insert(self,k):
+        self.heapList.append(k)
+        self.currentSize=self.currentSize+1
+        self.percUp(self.currentSize)
+
+    def percDown(self, i):
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
+
+    def minChild(self,i):
+        if i*2+1>self.currentSize:
+            return i*2
+        else:
+            if self.heapList[i*2+1]<self.heapList[i*2]:
+                return i*2+1
+            else:
+                return i*2
+
+    def delmin(self):
+        retval=self.heapList[1]
+        self.heapList[1]=self.heapList[self.currentSize]
+        self.currentSize=self.currentSize-1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
+
+    def buildHeap(self,alist):
+        i=len(alist)//2
+        self.currentSize=len(alist)
+        self.heapList=[0]+ alist[:]
+        while i>0:
+            self.percDown(i)
+            i=i-1
+
+bh = binaryheap()
+bh.buildHeap([9,5,6,2,3])
+
+print(bh.delmin())
+print(bh.delmin())
+
+print(bh.currentSize)
+bh2=binaryheap()
+bh2.insert(2)
+bh2.insert(3)
+bh2.insert(1)
+bh2.insert(7)
+bh2.insert(5)
+bh2.insert(6)
+print(bh2.currentSize)
+print(bh2.delmin())
